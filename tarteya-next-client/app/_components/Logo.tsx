f9a4fab826157ff9d0fcda7c66c8c0180cc6f1c9
sergey-sanches-peres <sanchesgithub@mail.ru>
2023-07-24 21:15:41 +0300

    logo

--- tarteya-next-client/app/_components/Logo.tsx
+++ tarteya-next-client/app/_components/Logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Logo = () =>{
     return(
         <Link href='/'>
-            <Image src='/tarteya_logo.svg' alt='logo' width={200} height={30}/>
+            <Image src='/tarteya_logo.svg' alt='tarteya-logo' width={200} height={30}/>
         </Link>
     );
 }
