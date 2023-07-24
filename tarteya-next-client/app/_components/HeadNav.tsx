ba5fdf5d8825a007b748017d8516ba69217c4b7b
sergey-sanches-peres <sanchesgithub@mail.ru>
2023-07-25 00:45:11 +0300

    added react scroller

--- tarteya-next-client/app/_components/HeadNav.tsx
+++ tarteya-next-client/app/_components/HeadNav.tsx
@@ -1,15 +1,16 @@
-import Link from "next/link";
+// import Link from "next/link";
+import { Link } from "react-scroll";
 
 const HeadNav = () =>{
     return(
         <nav className="header__nav">
-            <Link href={{pathname: '', hash: "1"}}>1</Link>
-            <Link href={{pathname: '', hash: "2"}}>2</Link>
-            <Link href={{pathname: '', hash: "3"}}>3</Link>
-            <Link href={{pathname: '', hash: "4"}}>4</Link>
-            <Link href={{pathname: '', hash: "5"}}>5</Link>
-            <Link href={{pathname: '', hash: "6"}}>6</Link>
-            <Link href={{pathname: '', hash: "7"}}>7</Link>
+            <Link activeClass="active__nav__item" to="1" spy={true} smooth={true} offset={-60} duration={500}>1</Link>
+            <Link activeClass="active__nav__item" to="2" spy={true} smooth={true} offset={-60} duration={500}>2</Link>
+            <Link activeClass="active__nav__item" to="3" spy={true} smooth={true} offset={-60} duration={500}>3</Link>
+            <Link activeClass="active__nav__item" to="4" spy={true} smooth={true} offset={-60} duration={500}>4</Link>
+            <Link activeClass="active__nav__item" to="5" spy={true} smooth={true} offset={-60} duration={500}>5</Link>
+            <Link activeClass="active__nav__item" to="6" spy={true} smooth={true} offset={-60} duration={500}>6</Link>
+            <Link activeClass="active__nav__item" to="7" spy={true} smooth={true} offset={-60} duration={500}>7</Link>
         </nav>
     );
 }
